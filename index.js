fa17d54a55a23c04fb7521c033c275030c7c87ce
Donald Reddy I <donaldreddy2712@gmail.com>
2023-10-06 12:22:19 +0530

    made mobile browser compatible

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 let colors = ["white", "blue", "red", "green", "yellow"];
 let cont = document.getElementsByClassName("container")[0];
-let level = 2000;
 let curScore = 0, highScore = localStorage.getItem("high_score") || 0;
 let missed = 0
 let popSound = new Audio("./sounds/pop.mp3");
 let gameOverSound = new Audio("./sounds/gameOver.wav");
 let screenWidth = screen.width > 600 ? 900 : 350;
+let backGroundMusic = new Audio("./sounds/background.mp3");
+
+document.addEventListener("click", () => {
+    setTimeout(playBG, 1000);
+}, { once: true });
+
+function playBG() {
+    backGroundMusic.play();
+    setInterval(playBG, 78000);
+}
 
 function rColor() {
     return colors[Math.floor(Math.random() * 5)];
 }
 
 function gameOver() {
+    backGroundMusic.volume = 0;
     gameOverSound.play();
     updateHighScore();
     cont.style.display = "flex";
@@ -29,11 +39,21 @@ function updateScore() {
     document.querySelector("#score span").innerText = curScore;
 }
 
-function popBubble(evnt) {
-    console.log(evnt.target);
-    evnt.target.remove();
-    level -= 50;
+function popBubble(event) {
+    event.target.remove();
     updateScore();
+}
+
+function inside(event) {
+    let coOrd = document.getElementsByClassName("bubble")[0].getBoundingClientRect();
+    let x1 = coOrd.left, x2 = coOrd.right, y1 = coOrd.top, y2 = coOrd.bottom;
+    let X = event.changedTouches[0].clientX, Y = event.changedTouches[0].clientY;
+    if ((x1 <= X && X <= x2) && (y1 <= Y && Y <= y2)) {
+        popSound.play();
+        document.getElementsByClassName("bubble")[0].remove();
+        updateScore();
+        addBubble();
+    }
 }
 
 function addBubble() {
@@ -47,10 +67,7 @@ function addBubble() {
     newDiv.style.left = `${Math.floor(Math.random() * screenWidth)}px`;
     cont.appendChild(newDiv);
 
-    if (screenWidth === 900)
-        newDiv.addEventListener("mouseenter", (evnt) => { popSound.play(); popBubble(evnt); addBubble(); });
-    else
-        newDiv.addEventListener("touchstart", (evnt) => { popSound.play(); popBubble(evnt); addBubble(); });
+    newDiv.addEventListener("mouseenter", (event) => { popSound.play(); popBubble(event); addBubble(); });
 
     newDiv.addEventListener("animationend", () => {
         newDiv.remove();
@@ -61,17 +78,28 @@ function addBubble() {
 }
 
 function start() {
-    level = 2000;
+    backGroundMusic.volume = 0.3
     curScore = 0;
     missed = 0;
     highScore = localStorage.getItem("high_score") || 0;
     updateHighScore();
-    console.log(localStorage.getItem("high_score") || 0);
-    cont.style.display = "block";
+
+    cont.addEventListener("touchstart", (event) => {
+        event.preventDefault();
+        inside(event);
+    });
+
+    cont.addEventListener("touchmove", (event) => {
+        event.preventDefault();
+        inside(event);
+    });
+
     document.getElementsByClassName("game-over")[0].style.display = "none";
     document.querySelector("#score span").innerText = curScore;
     document.querySelector("#missed span").innerText = missed;
+
     cont.style.display = "block";
+
     addBubble();
 }
 
